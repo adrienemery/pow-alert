da05c82876eedf4812f63a9923cb887b38eb9de1
Malcolm Dimeglio <malcolm.dimeglio@gmail.com>
2018-02-01 10:55:32 -0800

    from HTML scrapping to Json parsing

--- POWalert_malc.py
+++ POWalert_malc.py
@@ -5,9 +5,10 @@ from twilio.rest import Client
 from dotenv import load_dotenv, find_dotenv
 from urllib.request import urlopen
 import requests
+import re
+import json
 from bs4 import BeautifulSoup
-from datetime import datetime
-startTime = datetime.now()
+
 
 CYPRESS = "Cypress"
 WHISTLER = "Whistler - Blackomb"
@@ -17,20 +18,22 @@ load_dotenv(find_dotenv())
 
 class Resort:
 
-    def __init__(self, name, cam_url, info_url):
+    def __init__(self, name, cam_url=None, info_url=None):
         self.name = name or "default"
-        self.webcam_url = cam_url or "http://"
-        self.info_url = info_url or "http://"
+        self.webcam_url = cam_url
+        self.info_url = info_url
         self.webcam_img = None
         self._24hsnow = "???"
         self._12hsnow = "???"
 
     def update(self):
-        if self.name == CYPRESS:
+        if self.webcam_url:
             self.webcam_img = urlopen(self.webcam_url).read()
-            page = requests.get(self.info_url)
-            soup = BeautifulSoup(page.content, 'html.parser')
 
+        page = requests.get(self.info_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        if self.name == CYPRESS:
             all_div = soup.find_all('div', class_='weather-item clearfix')
             for div in all_div:
                 if "24 hr Snow" in div.text:
@@ -38,31 +41,24 @@ class Resort:
                     self._24hsnow = el.text
 
         if self.name == WHISTLER:
-            # Solution with other url and No selenium
-            page = requests.get(self.info_url)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            all_div = soup.find_all('div', id='conditions')
-            for el in all_div:
-                tab_snowfall = el.find("table", {"class": "tomtable"})
-            tab = []
-            for row in tab_snowfall.find_all('tr'):
-                for col in row.find_all('td'):
-                    if col.text:
-                        tab.append(col.text)
-            self._12hsnow = (tab[5].split(' '))[0]
-            self._24hsnow = (tab[6].split(' '))[0]
+            text_json = re.search('FR.snowReportData = ({.*});', page.text)
+            data = json.loads(text_json.groups()[0])
+            self._24hsnow = data['TwentyFourHourSnowfall']['Centimeters']
+            self._12hsnow = data['OvernightSnowfall']['Centimeters']
 
     def discard_info(self):
         print(self.name+" report:")
-        print(self._12hsnow+" overnight")
-        print(self._24hsnow + " last 24h")
+        print(self._12hsnow+" cm overnight")
+        print(self._24hsnow + " cm last 24h")
         print("*********")
 
     @property
     def data(self):
         self.update()
-        str = self.name+" report:\n"+self._12hsnow+" overnight\n"+self._24hsnow + " last 24h\n"+"******************\n"
-        return str
+        return self.name + " report:\n" + \
+               self._12hsnow + " cm overnight\n" + \
+               self._24hsnow + " cm last 24h\n" + \
+               "******************\n"
 
 
 Cypress = Resort(name=CYPRESS,
@@ -70,8 +66,7 @@ Cypress = Resort(name=CYPRESS,
                  info_url="http://www.cypressmountain.com/downhill-conditions/")
 
 Whistler = Resort(name=WHISTLER,
-                  cam_url="",
-                  info_url="https://www.pembertonvalleylodge.com/pemberton/snow-conditions/")
+                  info_url="https://www.whistlerblackcomb.com/the-mountain/mountain-conditions/snow-and-weather-report.aspx")
 
 text = Cypress.data + Whistler.data
 
